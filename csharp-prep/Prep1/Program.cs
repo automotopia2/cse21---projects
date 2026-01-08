2fedd5eb2980ae293b53066018efa97a46d3f6c9
automotopia2 <jessejbeers@gmail.com>
2026-01-07 22:12:57 -0700

    Completed all core requirements

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name? ");
-        string firstname = Console.ReadLine();
+        // James Bond name display
 
-        Console.Write("What is your last name? ");
-        string lastname = Console.ReadLine();
 
-        Console.WriteLine($"\nYour name is {lastname}, {firstname} {lastname}.");
+        // Console.Write("What is your first name? ");
+        // string firstname = Console.ReadLine();
+
+        // Console.Write("What is your last name? ");
+        // string lastname = Console.ReadLine();
+
+        // Console.WriteLine($"\nYour name is {lastname}, {firstname} {lastname}.");
+
+
+
+        // GRADE CALCULATOR //
+
+        // Recording grade percentage
+        Console.Write("Please enter your grade percentage here ");
+        string enteredNum = Console.ReadLine();
+
+        // Declaring variable and converting string to int
+        int gradeNum = int.Parse(enteredNum);
+        string Letter = "";
+        
+        // Convert percentage to letter grade
+        if (gradeNum >= 90)
+        {
+            Letter = "A";
+        }
+        else if (gradeNum >= 80)
+        {
+            Letter = "B";
+        }
+        else if (gradeNum >= 70)
+        {
+            Letter = "C";
+        }
+        else if (gradeNum >= 60)
+        {
+            Letter = "D";
+        }
+        else
+        {
+            Letter = "F";
+        }
+        Console.Write($"Your letter grade is {Letter}. ");
+
+        if (gradeNum >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed the class. Please try again next semester!");
+        }
     }
 }
