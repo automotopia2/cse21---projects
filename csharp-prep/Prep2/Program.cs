aa5c149a5c6bec3b90a9960f71cbc758f9f6f387
automotopia2 <jessejbeers@gmail.com>
2026-01-08 13:35:30 -0700

    I completed the core requirements and stretch challenges.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string play_again = "";
+        while (play_again != "no")
+        {
+        // Console.Write("What would you like the magic number to be? ");
+        // int magic_num = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magic_num = randomGenerator.Next(1, 100);
+        int guess = 999;
+        int try_tracker = 0;
+
+        while (guess != magic_num)
+        {
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+        try_tracker = try_tracker + 1;
+        if (guess == magic_num)
+        {
+            Console.WriteLine($"Correct! The magic number is {magic_num}");
+        }
+        else if (guess < magic_num)
+        {
+            Console.WriteLine("Guess a higher number.");
+        }
+        else if (guess > magic_num)
+        {
+            Console.WriteLine("Guess a lower number.");
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid number.");
+        }
+        }
+        Console.WriteLine($"It took you {try_tracker} guesses!");
+
+        Console.Write("Would you like to play again? Please enter 'yes' or 'no' here: ");
+        play_again = Console.ReadLine();        
+        }
+        Console.WriteLine("Thank you for playing! Have a wonderful day!");
     }
 }
