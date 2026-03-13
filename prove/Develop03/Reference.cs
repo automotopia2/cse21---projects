57bd2f80676c82a100b3eb7ab1e3994029246039
automotopia2 <jessejbeers@gmail.com>
2026-03-13 15:11:39 -0600

    Completed Reference class.

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Reference 
+{
+    // Variables 
+    private string _location;
+    private string _text;
+    
+    // Methods
+    public Reference(string location, string text)
+    {
+        _location = location;
+        _text = text;
+    }
+}
