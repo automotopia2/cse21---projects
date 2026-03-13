8462b63d2e4ad3bf96b64400f23b3235d8e49260
automotopia2 <jessejbeers@gmail.com>
2026-03-13 16:18:27 -0600

    Finished Word class.

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Word
+{
+    // Variables
+    private string _text;
+    private bool _isHidden;
+    private Random _random;
+
+    // Methods
+    public Word(string text)
+    {
+        _text = text;
+    }
+    public void Hide()
+    {
+        
+    }
+    public void IsHidden()
+    {
+        
+    }
+}
