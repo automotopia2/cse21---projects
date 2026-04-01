ccdbb5d89e19d7207ff1c7921a4030e8367e716c
automotopia2 <jessejbeers@gmail.com>
2026-04-01 14:16:31 -0600

    Completed all classes. Program needs to be built to run classes.

---
+++ final/Foundation2/Customer.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Customer
+{
+    // Variables
+    private string _name;
+    // private Address address = new Address("2645 South Quenzer Way", "Middleton", "Idaho", "USA");
+    private Address _address;
+    // Constructor
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
+    // Methods
+}
