6a10f343384b837c703af3a19326abe7c4e3d2f2
automotopia2 <jessejbeers@gmail.com>
2026-03-31 21:49:39 -0600

    Added all classes and functions. Just needs inputs for the comments.

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        VideoLibrary videos = new VideoLibrary();
+        Video video1 = new Video("I Love Snakes", "Jimmy Carlo", 200);
+        Comment comment1 = new Comment("","");
+        Comment comment2 = new Comment("","");
+        Comment comment3 = new Comment("","");
+
+        Video video2 = new Video("I Hate Snakes", "Joe Como", 500);
+        Comment comment4 = new Comment("","");
+        Comment comment5 = new Comment("","");
+        Comment comment6 = new Comment("","");
+
+        Video video3 = new Video("I Want Frogs", "Hailee Bailee", 450);
+        Comment comment7 = new Comment("","");
+        Comment comment8 = new Comment("","");
+        Comment comment9 = new Comment("","");
+        
     }
 }
